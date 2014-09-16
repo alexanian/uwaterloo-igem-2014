993137cb3bc43ca69ebe678dc3054f00ece462be
Tessa <symbiologer@gmail.com>
2014-09-16 14:15:19 -0400

    Fixes brackets around the Hill coefficient

--- modelling/crispr_model.m
+++ modelling/crispr_model.m
@@ -105,7 +105,7 @@ function dS=crispr_ode(t,s)
     mrnaC_dt= alpha_mrnaC - gamma_mrnaC*mrnaC;
     C_dt= beta_C*mrnaC - gamma_C*C + k_minus*CRg - k_plus*C*Rg;
     Rg_dt=alpha_Rg - gamma_Rg*Rg + k_minus*CRg - k_plus*C*Rg;
-    mrnaY_dt=alpha_mrnaY/(1 + CRg/K_a)^n - gamma_mrnaY*mrnaY + alpha0_mrnaY;
+    mrnaY_dt=alpha_mrnaY/(1 + (CRg/K_a)^n) - gamma_mrnaY*mrnaY + alpha0_mrnaY;
 
     dS=[mrnaC_dt, C_dt, Rg_dt, mrnaY_dt]';
 end
