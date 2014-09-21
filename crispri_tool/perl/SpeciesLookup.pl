1cd56f996726e6f01ee1afb098b21ed8a501f804
Rollen D'Souza <rocknrollen@gmail.com>
2014-09-21 17:28:48 -0400

    Fixed SpeciesLookup tool and added new GeneLookup Tool
    Usage:
        perl GeneLookup.pl ExactSpeciesName DescriptionLookup

--- crispri_tool/perl/SpeciesLookup.pl
+++ crispri_tool/perl/SpeciesLookup.pl
@@ -8,8 +8,12 @@ my $temp = 'staphylococcus_aureus_.*';
 my @dbas = @{$lookup->get_all_by_name_pattern(@ARGV[0] . ".*")};
 my $limit = 0 + @ARGV[1];
 
-print "{\n";
-for( my $c = 0; ( $c < scalar(@dbas) ) and ( $c < $limit ); $c++ ) { 
-    print "{ name:\"" . @dbas[$c]->species() . "\", id:\"" . @dbas[$c]->species_id . "\"}\n";
+print "[\n";
+my $c = 0;
+Species: while( scalar(@dbas) ) { 
+    print "{ name:\"" . @dbas[$c]->species() . "\", id:\"" . @dbas[$c]->species_id . "\"}";
+    $c++;
+  if( ($c >= scalar(@dbas) ) or ( $c >= $limit ) ) { last Species; }
+    print ",\n"
 }
-print "}";
+print "\n]";
