6575ad6af755f5ee21eb63987e63a7f6d2863eaf
Rollen D'Souza <rocknrollen@hotmail.com>
2014-10-02 11:23:17 -0400

    Incorporate changes to repression in system file and data for sensitivity analysis.

--- modelling/CRISPRi_System.m
+++ modelling/CRISPRi_System.m
@@ -3,33 +3,36 @@ function dS = CRISPRi_System( t, s, p )
     alpha_mrnaC = p(1);
     alpha_mrnaY = p(2);
     alpha_Rg = p(3);
-    alpha0_mrnaY = p(4);
-    beta_C = p(5);
-    gamma_C = p(6);
+    beta_C = p(4);
+    gamma_C = p(5);
+    gamma_B = p(6);
     gamma_mrnaC = p(7);
     gamma_Rg = p(8);
     gamma_mrnaY = p(9);
 
     % Reaction rate constants
-    K = p(10);
-    K_a = p(11);
-    n = p(12);
-    
+    k_minus = p(10);
+    k_plus = p(11);
+    K_a = p(12);
+    n = p(13);
+
     % Differential terms
     mrnaC=s(1); % dCas9 mRNA
     C=s(2); % dCas9
     Rg=s(3); % sgRNA
     mrnaY=s(4); % mRNA to produce YFP
+    mrnaY_noR=s(5); % mRNA dynamics without repression
 
     % Non-differential terms from QSSA
-    CRg = K*C*Rg;
+    B = k_plus*C*Rg/(k_minus+gamma_B);
 
-    % Actual System
-    mrnaC_dt=alpha_mrnaC - gamma_mrnaC*mrnaC;
-    C_dt= beta_C*mrnaC - gamma_C*C;
-    Rg_dt=alpha_Rg - gamma_Rg*Rg;
-    mrnaY_dt=alpha_mrnaY/(1 + (CRg/K_a)^n) - gamma_mrnaY*mrnaY + alpha0_mrnaY;
-
-    dS=[mrnaC_dt, C_dt, Rg_dt, mrnaY_dt]';
+    % Differential System
+    mrnaC_dt= alpha_mrnaC - gamma_mrnaC*mrnaC;
+    C_dt= beta_C*mrnaC - gamma_C*C + k_minus*B - k_plus*C*Rg;
+    Rg_dt=alpha_Rg - gamma_Rg*Rg + k_minus*B - k_plus*C*Rg;
+    mrnaY_dt=alpha_mrnaY*(0.6 + 0.4/(1 + (B/K_a)^n)) - gamma_mrnaY*mrnaY;
+    mrnaY_noR_dt=alpha_mrnaY - gamma_mrnaY*mrnaY_noR;
+    
+    dS=[mrnaC_dt, C_dt, Rg_dt, mrnaY_dt, mrnaY_noR_dt]';
 end
 
