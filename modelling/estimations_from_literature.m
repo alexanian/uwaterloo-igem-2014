8d50136ac02cad27c510d43e45d9623c041bfc22
Tessa <symbiologer@gmail.com>
2014-10-06 23:26:17 -0400

    Converts to nM units for SarA estimation graph, accounts for polysomes in beta_C rates

--- modelling/estimations_from_literature.m
+++ modelling/estimations_from_literature.m
@@ -1,4 +1,6 @@
+%%%
 % Estimation of mRNA production from SarA promoter
+%%%
 % Fluorescence (OD) read from Cheung et al, see figure 4B in
 % http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2258893/
 % Values for relative fluorescence measured super-hackily by interpolating the
@@ -11,27 +13,36 @@ fluor_OD_sarA = [9500 10590 10700 11310 12420 13400 14000 15080];
 % I then converted from fluorescence to number of molecules
 % using Wu & Pollard: 0.00676 A.U / mean # molecules per cell
 fluor_molec = fluor_OD_sarA / 0.00676;
+fluor_nanomol = fluor_molec / (6.0331415e23 * 10e-9);
+
+% Convert to a per-concentration rate
+staph_cell_volume_l = 4/3*pi*1.1e-6^3 * 1000;
+fluor_nM = fluor_nanomol / staph_cell_volume_l;
 
 % Curve fitting of exponent
 ft=fittype('exp1');
-expCoeff = coeffvalues(fit(time_min',fluor_molec',ft));
+expCoeff = coeffvalues(fit(time_min',fluor_nM',ft));
 expFit = expCoeff(1)*exp(expCoeff(2)*time_min);
+sarA_per_mol_rate = expCoeff(2);
 
-h = plot(time_min, fluor_molec, 'kd', time_min,expFit, 'b');
+h = plot(time_min, fluor_nM, 'kd', time_min,expFit, 'b');
 set(h, {'MarkerFaceColor'}, {'k'; 'b';}, {'MarkerSize'}, {6; 1;},...
     {'LineWidth'},{1; 2;})
-xlabel('Time (min)'); ylabel('# of Fluorescent Molecules');
+xlabel('Time (min)'); ylabel('Fluorescent Molecules in Cell (nM)');
 xlim([180 600]); 
 
-sarA_per_molecule_rate = expCoeff(2)
-
+%%%
 % Estimation of dCas9 protein degradation rate
+%%%
 % Half-life of SarA protein in S Aureus given in Michalik et al
 % http://www.mcponline.org/content/11/9/558.abstract
-half_life_h = 20.48;
-gamma_C = -log(2)/(half_life_h*60);
+halfLife_h = 20.48;
+gamma_C = -log(2)/(halfLife_h*60);
 
+%%%
 % Estimation of dCas9 translation rate
+%%%
 peptide_s = [0.59 3.17];
 dCas9_s = peptide_s / 1372;
-beta_C = dCas9_s * 60;
+dCas9_m = dCas9_s * 60;
+beta_C = [dCas9_m(1)*0.22 dCas9_m(2)*3.46]
