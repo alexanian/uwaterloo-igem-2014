d743c806722ec935f12e19c9908c4a9f7f13cf58
Tessa <symbiologer@gmail.com>
2014-10-16 22:20:13 -0400

    Bugfix: exponential model now actually has 35 minute half life

--- modelling/Simulate_CRISPRi_Time_Series.m
+++ modelling/Simulate_CRISPRi_Time_Series.m
@@ -1,6 +1,8 @@
 function YD = Simulate_CRISPRi_Time_Series( t_min, Y0, FR )
-    % Define exponential using data from Qi et al. 2013
-    YD = Y0*exp(-(t_min-8)/35);
+    % Define exponential using 35-minute half life from Qi et al. 2013 
+    lambda = log(2)/35;
+    
+    YD = Y0*exp(-lambda*(t_min-8));
     YD(YD > Y0) = Y0; 
     YD(YD < Y0/FR) = Y0/FR;
     YD = YD';
