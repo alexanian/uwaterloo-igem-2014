76a84ec99d30ba5b2e370b122c76ef92fcffff8f
Tessa <symbiologer@gmail.com>
2014-10-16 22:21:30 -0400

    Plot function specifies less (so is more flexible)

--- modelling/Simulate_CRISPRi_Error.m
+++ modelling/Simulate_CRISPRi_Error.m
@@ -1,7 +1,7 @@
-function Error = Simulate_CRISPRi_Error( t_min, YD , Y0, Parameters )
+function Error = Simulate_CRISPRi_Error( t_min, YD , S0, Parameters )
 
     DE = @(T,S) CRISPRi_System( T, S, Parameters );
-    [~, YDExperiment] = ode23( DE, t_min, Y0 );
+    [~, YDExperiment] = ode23( DE, t_min, S0 );
     YDOut = CRISPRi_System_Output(YDExperiment);
     
     E = abs(YDOut - YD);
